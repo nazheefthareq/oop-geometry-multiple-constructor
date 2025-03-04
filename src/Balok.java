959823a6b6114f0dc4f5fba9b40f3c75ff399952
Nazheef Thareq <nazheefthareq15@gmail.com>
2025-03-04 10:38:56 +0700

    add: set-multiple constructor-main for block

--- src/Balok.java
+++ src/Balok.java
@@ -1,3 +1,31 @@
 public class Balok {
 
+    public double panjang, lebar, tinggi, volume;
+
+    // using set
+    public void setPanjang(double panjang) {
+        this.panjang = panjang;
+    }
+    public void setLebar(double lebar) {
+        this.lebar = lebar;
+    }
+    public void setTinggi(double tinggi) {
+        this.tinggi = tinggi;
+    }
+
+    // using multiple constructor
+    Balok() {
+
+    }
+
+    Balok(double panjang, double lebar, double tinggi) {
+        this.panjang = panjang;
+        this.lebar = lebar;
+        this.tinggi = tinggi;
+    }
+
+    public double ComputeAndSetVolume() {
+        double volume = this.panjang * this.lebar * this.tinggi;
+        return volume;
+    }
 }
--- src/MainBangunRuang.java
+++ src/MainBangunRuang.java
@@ -5,9 +5,21 @@ public class MainBangunRuang {
         cube1.setSisi(2.5);
         cube1.ComputeAndSetVolume();
         System.out.println("Volume kubus 1 adalah:"+ cube1.ComputeAndSetVolume());
-        System.out.println("=============================================================");
+        System.out.println("-------------------------------------------------------------");
         Kubus cube2 = new Kubus(2.5);
         cube2.ComputeAndSetVolume();
-        System.out.println("Volume kubus 2 adalah"+ cube2.ComputeAndSetVolume());
+        System.out.println("Volume kubus 2 (constructor) adalah:"+ cube2.ComputeAndSetVolume());
+
+        System.out.println("========================== BALOK ============================");
+        Balok block1 = new Balok();
+        block1.setPanjang(4);
+        block1.setLebar(2.5);
+        block1.setTinggi(3);
+        block1.ComputeAndSetVolume();
+        System.out.println("Volume balok 1 adalah:"+ block1.ComputeAndSetVolume());
+        System.out.println("-------------------------------------------------------------");
+        Balok block2 = new Balok(4,2.5,3);
+        block2.ComputeAndSetVolume();
+        System.out.println("Volume balok 2 (constructor) adalah:"+ block2.ComputeAndSetVolume());
     }
 }
