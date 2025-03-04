ac4e46f6f2db2d1218c6d638258e466de981195e
Nazheef Thareq <nazheefthareq15@gmail.com>
2025-03-04 10:21:49 +0700

    add: set-multiple constructor-main for cube

--- src/Kubus.java
+++ src/Kubus.java
@@ -1,3 +1,23 @@
 public class Kubus {
-    
+
+    public double sisi, volume;
+
+    // using set value
+    public void setSisi(double sisi) {
+        this.sisi = sisi;
+    }
+
+    // using multiple constructor
+    Kubus() {
+
+    }
+
+    Kubus(double sisi) {
+        this.sisi = sisi;
+    }
+
+    public double ComputeAndSetVolume() {
+        double volume = this.sisi * this.sisi * this.sisi;
+        return volume;
+    }
 }
--- src/MainBangunRuang.java
+++ src/MainBangunRuang.java
@@ -1,5 +1,13 @@
 public class MainBangunRuang {
     public static void main(String[] args){
-        
+        System.out.println("========================== KUBUS ============================");
+        Kubus cube1 = new Kubus();
+        cube1.setSisi(2.5);
+        cube1.ComputeAndSetVolume();
+        System.out.println("Volume kubus 1 adalah:"+ cube1.ComputeAndSetVolume());
+        System.out.println("=============================================================");
+        Kubus cube2 = new Kubus(2.5);
+        cube2.ComputeAndSetVolume();
+        System.out.println("Volume kubus 2 adalah"+ cube2.ComputeAndSetVolume());
     }
 }
